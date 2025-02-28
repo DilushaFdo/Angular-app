72ff86190817e5b8af27d0bb8eb98b9706b406f0
DilushaFdo <130301311+DilushaFdo@users.noreply.github.com>
2025-02-28 18:09:57 +0530

    Succesfully completed the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
-import { NgFor } from '@angular/common';
-import { NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
